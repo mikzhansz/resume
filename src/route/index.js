25c5a67124f7746091a479b1c9bbc4fd0dad07ed
mikzhansz <m.zhansz@gmail.com>
2024-02-15 20:19:59 +0200

    upd master

--- src/route/index.js
+++ src/route/index.js
@@ -1065,6 +1065,15 @@ res.render('person', {
           //                  ↑↑ сюди вводимо JSON дані
         })
    
+        router.get('/bootstrap', function (req, res) {
+          // res.render генерує нам HTML сторінку
+        
+          //            ↙ cюди вводимо назву файлу з сontainer
+          res.render('bootstrap', {
+            layout:'bootstrap',
+          })
+          //                  ↑↑ сюди вводимо JSON дані
+        })        
   
 // Підключаємо роутер до бек-енду
 module.exports = router
